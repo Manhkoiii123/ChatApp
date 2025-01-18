d969cf7a1a7c7e20131b18760a191caaadea35a0
manhtranduc <manhtranduc0202@gmail.com>
2025-01-18 19:54:26 +0700

    profile

--- server/routes/AuthRoutes.js
+++ server/routes/AuthRoutes.js
@@ -1,6 +1,25 @@
 import { Router } from "express";
-import { login, signUp } from "../controllers/AuthController.js";
+import {
+  getUserInfo,
+  login,
+  signUp,
+  updateProfile,
+  addProfileImage,
+  removeProfileImage,
+} from "../controllers/AuthController.js";
+import { verifyToken } from "../middlewares/AuthMiddleware.js";
+import multer from "multer";
+const upload = multer({ dest: "uploads/profiles/" });
 const authRoutes = Router();
 authRoutes.post("/signup", signUp);
 authRoutes.post("/login", login);
+authRoutes.get("/user-info", verifyToken, getUserInfo);
+authRoutes.post("/update-profile", verifyToken, updateProfile);
+authRoutes.post(
+  "/add-profile-image",
+  verifyToken,
+  upload.single("profile-image"),
+  addProfileImage
+);
+authRoutes.delete("/remove-profile-image", verifyToken, removeProfileImage);
 export default authRoutes;
