ebc5614e998246f9154a90eda5536c76549873af
manhtranduc <manhtranduc0202@gmail.com>
2025-01-19 12:54:29 +0700

    profile

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,82 @@
+import { apiClient } from "@/lib/api-client";
 import Auth from "@/pages/auth";
 import Chat from "@/pages/chat";
 import Profile from "@/pages/profile";
+import { createUserState, useAuthZustand } from "@/store/slices/authSlice";
+import { GET_USER_INFO } from "@/utils/constants";
+import { useEffect, useState } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-
+const PrivateRoute = ({ children }: { children: JSX.Element }) => {
+  const user = useAuthZustand((state: createUserState) => state.user);
+  const isAuthenticated = !!user;
+  return isAuthenticated ? children : <Navigate to="/auth" />;
+};
+const AuthRoute = ({ children }: { children: JSX.Element }) => {
+  const user = useAuthZustand((state: createUserState) => state.user);
+  const isAuthenticated = !!user;
+  return isAuthenticated ? <Navigate to="/chat" /> : children;
+};
 function App() {
+  const user = useAuthZustand((state: createUserState) => state.user);
+  const setUser = useAuthZustand((state: createUserState) => state.setUser);
+  const removeUser = useAuthZustand(
+    (state: createUserState) => state.removeUser
+  );
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getUserData = async () => {
+      try {
+        const res = await apiClient.get(GET_USER_INFO, {
+          withCredentials: true,
+        });
+        if (res.status === 200 && res.data.id) {
+          setUser(res.data);
+        } else {
+          removeUser();
+        }
+      } catch (error) {
+        console.log(error);
+        setLoading(false);
+      } finally {
+        setLoading(false);
+      }
+    };
+    if (!user) {
+      getUserData();
+    } else {
+      setLoading(false);
+    }
+  }, [removeUser, setUser, user]);
+  if (loading) return <div>Loading...</div>;
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/auth" element={<Auth />} />
-        <Route path="/chat" element={<Chat />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route
+          path="/auth"
+          element={
+            <AuthRoute>
+              <Auth />
+            </AuthRoute>
+          }
+        />
+        <Route
+          path="/chat"
+          element={
+            <PrivateRoute>
+              <Chat />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <PrivateRoute>
+              <Profile />
+            </PrivateRoute>
+          }
+        />
         <Route path="/*" element={<Navigate to="/auth" />} />
       </Routes>
     </BrowserRouter>
--- client/src/pages/chat/index.tsx
+++ client/src/pages/chat/index.tsx
@@ -1,4 +1,17 @@
+import { createUserState, useAuthZustand } from "@/store/slices/authSlice";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
+
 const Chat = () => {
+  const user = useAuthZustand((state: createUserState) => state.user);
+  const nav = useNavigate();
+  useEffect(() => {
+    if (!user?.profileSetup) {
+      toast("Please setup your profile first");
+      nav("/profile");
+    }
+  }, [nav, user]);
   return <div>Chat</div>;
 };
 
