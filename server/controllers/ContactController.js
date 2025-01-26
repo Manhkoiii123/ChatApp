2dbb11e632948ecf82b4d4ed80bf734563610f12
manhtranduc <manhtranduc0202@gmail.com>
2025-01-26 10:03:19 +0700

    get_contact

--- server/controllers/ContactController.js
+++ server/controllers/ContactController.js
@@ -1,4 +1,6 @@
+import mongoose from "mongoose";
 import User from "../models/UserModel.js";
+import Message from "../models/MessageModel.js";
 export const searchContacts = async (req, res, next) => {
   try {
     const { searchTerm } = req.body;
@@ -20,6 +22,63 @@ export const searchContacts = async (req, res, next) => {
     });
     return res.status(200).json({ contacts });
   } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+export const getContactsForDMList = async (req, res, next) => {
+  try {
+    let { userId } = req;
+    userId = new mongoose.Types.ObjectId(userId);
+    const contacts = await Message.aggregate([
+      {
+        $match: {
+          $or: [{ sender: userId }, { recipient: userId }],
+        },
+      },
+      {
+        $sort: { timestamps: -1 },
+      },
+      {
+        $group: {
+          _id: {
+            $cond: {
+              if: { $eq: ["$sender", userId] },
+              then: "$recipient",
+              else: "$sender",
+            },
+          },
+          lastMessageTime: { $first: "$timestamps" },
+        },
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "_id",
+          foreignField: "_id",
+          as: "contactInfo",
+        },
+      },
+      {
+        $unwind: "$contactInfo",
+      },
+      {
+        $project: {
+          _id: 1,
+          lastMessageTime: 1,
+          email: "$contactInfo.email",
+          firstName: "$contactInfo.firstName",
+          lastName: "$contactInfo.lastName",
+          image: "$contactInfo.image",
+          color: "$contactInfo.color",
+        },
+      },
+      {
+        $sort: { lastMessageTime: -1 },
+      },
+    ]);
+    return res.status(200).json({ contacts });
+  } catch (error) {
+    console.log("🚀 ~ getContactsForDMList ~ error:", error);
     return res.status(500).json({ message: error.message });
   }
 };
